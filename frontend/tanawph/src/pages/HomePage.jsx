5b2841ee4f27a9c75695f8728616bb4ed6560683
Earl Nercua <andreinercua7896@gmail.com>
2025-08-30 22:05:01 +0800

    Adjusted HomePage.jsx img src

--- frontend/tanawph/src/pages/HomePage.jsx
+++ frontend/tanawph/src/pages/HomePage.jsx
@@ -54,17 +54,17 @@ const HomePage = () => {
 
 
             <div className="w-1/3 flex flex-col items-center justify-center">
-              <img className='size-50 w-auto' src="../src/assets/images/image_hero1.svg"/>
+              <img className='size-50 w-auto' src="./assets/images/image_hero1.svg"/>
               <h4 className='text-2xl font-bold text-center bg-[linear-gradient(to_right,#EB5E55,#FFC758)] bg-clip-text text-transparent pb-8'>4 Volunteers In Action</h4>
             </div>
             
             <div className="w-1/3 flex flex-col items-center justify-center">
-              <img className='size-50 w-auto' src="../src/assets/images/image_hero2.svg"/>
+              <img className='size-50 w-auto' src="./assets/images/image_hero2.svg"/>
               <h4 className='w-sm text-2xl font-bold text-center bg-[linear-gradient(to_right,#EB5E55,#FFC758)] bg-clip-text text-transparent pb-8'>+1M Unique User Reached</h4>
             </div>
 
             <div className="w-1/3 flex flex-col items-center justify-center">
-              <img className='size-50 w-auto' src="../src/assets/images/image_hero3.svg"/>
+              <img className='size-50 w-auto' src="./assets/images/image_hero3.svg"/>
               <h4 className='text-2xl font-bold text-center bg-[linear-gradient(to_right,#EB5E55,#FFC758)] bg-clip-text text-transparent pb-8'>5 Min. Average Session Time</h4>
             </div>
 
@@ -75,35 +75,35 @@ const HomePage = () => {
         <h1 className='text-4xl font-bold text-center pb-8'>How We Keep It 100% Verified.</h1>
         <div className="w-2xl grid  gap-y-4">
           <div className="flex w-full text-white bg-[linear-gradient(to_right,#EB5E55,#FFC758)] rounded-full px-8 py-4">
-            <img className='h-full 'src="../src/assets/images/image_homeverified1.png" alt="" />
+            <img className='h-full 'src="./assets/images/image_homeverified1.png" alt="" />
             <div>
               <h4 className="font-bold text-xl">Research</h4>
               <p>Our team gathers information only from credible sources like government sites, news, and public records.</p>
             </div>
           </div>
           <div className="flex w-full text-white bg-[linear-gradient(to_right,#EB5E55,#FFC758)] rounded-full px-8 py-4">
-            <img className='h-full 'src="../src/assets/images/image_homeverified2.png" alt="" />
+            <img className='h-full 'src="./assets/images/image_homeverified2.png" alt="" />
             <div>
               <h4 className="font-bold text-xl">Verification</h4>
               <p>Every fact is double-checked by our head researchers to confirm accuracy. </p>
             </div>
           </div>
           <div className="flex w-full text-white bg-[linear-gradient(to_right,#EB5E55,#FFC758)] rounded-full px-8 py-4">
-            <img className='h-full 'src="../src/assets/images/image_homeverified3.png" alt="" />
+            <img className='h-full 'src="./assets/images/image_homeverified3.png" alt="" />
             <div>
               <h4 className="font-bold text-xl">Simplified for You</h4>
               <p>We rewrite complex data into clear, easy-to-read summaries without losing accuracy.</p>
             </div>
           </div>
           <div className="flex w-full text-white bg-[linear-gradient(to_right,#EB5E55,#FFC758)] rounded-full px-8 py-4">
-            <img className='h-full 'src="../src/assets/images/image_homeverified4.png" alt="" />
+            <img className='h-full 'src="./assets/images/image_homeverified4.png" alt="" />
             <div>
               <h4 className="font-bold text-xl">Citation</h4>
               <p>Each piece of information comes with a source link so you can check it yourself.</p>
             </div>
           </div>
           <div className="flex w-full text-white bg-[linear-gradient(to_right,#EB5E55,#FFC758)] rounded-full px-8 py-4">
-            <img className='h-full 'src="../src/assets/images/image_homeverified5.png" alt="" />
+            <img className='h-full 'src="./assets/images/image_homeverified5.png" alt="" />
             <div>
               <h4 className="font-bold text-xl">Regular Updates</h4>
               <p>We keep track of changes and update the platform so you’re always looking at the latest information.</p>
@@ -134,10 +134,10 @@ const HomePage = () => {
                 <h4 className='font-bold text-2xl bg-[linear-gradient(to_right,#EB5E55,#FFC758)] bg-clip-text text-transparent'>Officials & Candidates</h4>
                 <p>Search verified profiles of leaders, parties, and candidates with their background, experience, and advocacies.</p>
               </div>
-              <img src="../src/assets/images/image_homeoffer1.svg" alt="" />
+              <img src="./assets/images/image_homeoffer1.svg" alt="" />
             </div>
             <div className="flex">
-              <img src="../src/assets/images/image_homeoffer2.svg" alt="" />
+              <img src="./assets/images/image_homeoffer2.svg" alt="" />
               <div className="flex-1 ml-8">
                 <h4 className='font-bold text-2xl bg-[linear-gradient(to_right,#EB5E55,#FFC758)] bg-clip-text text-transparent'>MIL Resources</h4>
                 <p>Access infographics, classroom-ready games, and real cases of digital dangers to learn smarter media habits.</p>
@@ -148,10 +148,10 @@ const HomePage = () => {
                 <h4 className='font-bold text-2xl bg-[linear-gradient(to_right,#EB5E55,#FFC758)] bg-clip-text text-transparent'>Fact Check Tools</h4>
                 <p>Search verified profiles of leaders, parties, and candidates with their background, experience, and advocacies.</p>
               </div>
-              <img src="../src/assets/images/image_homeoffer2.svg" alt="" />
+              <img src="../assets/images/image_homeoffer2.svg" alt="" />
             </div>
             <div className="flex">
-              <img src="../src/assets/images/image_homeoffer2.svg" alt="" />
+              <img src="../assets/images/image_homeoffer2.svg" alt="" />
               <div className="flex-1 ml-8">
                 <h4 className='font-bold text-2xl bg-[linear-gradient(to_right,#EB5E55,#FFC758)] bg-clip-text text-transparent'>Youth Empowerment</h4>
                 <p>Access infographics, classroom-ready games, and real cases of digital dangers to learn smarter media habits.</p>
@@ -162,7 +162,7 @@ const HomePage = () => {
                 <h4 className='font-bold text-2xl bg-[linear-gradient(to_right,#EB5E55,#FFC758)] bg-clip-text text-transparent'>Tawi Assistant</h4>
                 <p>Search verified profiles of leaders, parties, and candidates with their background, experience, and advocacies.</p>
               </div>
-              <img className='h-full' src="../src/assets/images/image_homeoffer3.svg" alt="" />
+              <img className='h-full' src="../assets/images/image_homeoffer3.svg" alt="" />
             </div>
 
           </div>
