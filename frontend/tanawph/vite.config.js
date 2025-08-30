f0726e86bfa735c3d46816ddf30b271639f45533
Earl Nercua <andreinercua7896@gmail.com>
2025-08-30 21:44:20 +0800

    fix10

--- frontend/tanawph/vite.config.js
+++ frontend/tanawph/vite.config.js
@@ -16,7 +16,18 @@ export default defineConfig({
           src: 'src/assets/images',
           dest: 'assets'
         }
-    ]})
+    ]}),
+    {
+      name: 'rewrite-src-paths',
+      enforce: 'post',
+      generateBundle(_, bundle) {
+        for (const file of Object.values(bundle)) {
+          if (file.type === 'asset' && file.fileName.endsWith('.html')) {
+            file.source = file.source.replace(/\/src\/assets\//g, './assets/');
+          }
+        }
+      }
+    }
   ],
   // base: process.env.VITE_BASE_PATH || '/tanawph'
   base: './',
